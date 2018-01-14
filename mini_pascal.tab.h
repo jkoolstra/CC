2642a3d358c8622892a3d57eebeed9a8bead075f
Lars Holdijk <larsholdijk@gmail.com>
2018-01-14 13:15:17 +0100

    Late commit with lab 3b done

---
+++ mini_pascal.tab.h
@@ -0,0 +1,115 @@
+/* A Bison parser, made by GNU Bison 2.3.  */
+
+/* Skeleton interface for Bison's Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     PROGRAM = 258,
+     FUNCTION = 259,
+     PROCEDURE = 260,
+     BEG = 261,
+     END = 262,
+     IF = 263,
+     THEN = 264,
+     ELSE = 265,
+     WHILE = 266,
+     DO = 267,
+     VAR = 268,
+     ARRAY = 269,
+     OF = 270,
+     ARRSEP = 271,
+     INTEGER = 272,
+     REAL = 273,
+     ASSIGNOP = 274,
+     RELOP = 275,
+     ADDOP = 276,
+     I_MULOP = 277,
+     R_MULOP = 278,
+     INUM = 279,
+     RNUM = 280,
+     ID = 281
+   };
+#endif
+/* Tokens.  */
+#define PROGRAM 258
+#define FUNCTION 259
+#define PROCEDURE 260
+#define BEG 261
+#define END 262
+#define IF 263
+#define THEN 264
+#define ELSE 265
+#define WHILE 266
+#define DO 267
+#define VAR 268
+#define ARRAY 269
+#define OF 270
+#define ARRSEP 271
+#define INTEGER 272
+#define REAL 273
+#define ASSIGNOP 274
+#define RELOP 275
+#define ADDOP 276
+#define I_MULOP 277
+#define R_MULOP 278
+#define INUM 279
+#define RNUM 280
+#define ID 281
+
+
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef union YYSTYPE
+#line 37 "mini_pascal.y"
+{
+    StrtabIndexList indexList;
+    ParameterList parameterList;
+    Type type;
+    TypeList typeList;
+}
+/* Line 1529 of yacc.c.  */
+#line 108 "mini_pascal.tab.h"
+	YYSTYPE;
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+# define YYSTYPE_IS_TRIVIAL 1
+#endif
+
+extern YYSTYPE yylval;
+
